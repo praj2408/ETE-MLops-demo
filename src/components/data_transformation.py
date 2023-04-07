f583c514224f5984585d77b909e4358439555151
Prajwal Krishna <prajwalgbdr03@gmail.com>
2023-04-07 22:38:39 +0530

    data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,7 +8,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 
@@ -30,11 +30,8 @@ class DataTransformation:
         """
         try:
             numerical_columns = ["age","bmi","children"]
-            categorical_columns = [
-                "gender",
-                "smoker",
-                "region",
-            ]
+            categorical_columns = ["gender","smoker","region"]
+            
 
             num_pipeline=Pipeline(
                 steps=[
@@ -46,7 +43,8 @@ class DataTransformation:
             cat_pipeline=Pipeline(
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("label_encoder",LabelEncoder())
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
                 ]
             )
 
@@ -72,7 +70,7 @@ class DataTransformation:
 
             logging.info("Read train and test data completed")
 
-            logging.info("Obtraining preprocessing object")
+            logging.info("Obtaining preprocessing object")
 
             preprocessing_obj=self.get_data_transformer_object()
 
